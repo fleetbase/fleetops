1b3647400980a208373e1e3d79c93781c8d145e0
TemuulenBM <temuulen.developer@gmail.com>
2023-11-08 18:09:35 +0800

    issue filters

---
+++ tests/unit/utils/get-issue-types-test.js
@@ -0,0 +1,10 @@
+import getIssueTypes from 'dummy/utils/get-issue-types';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | get-issue-types', function () {
+    // TODO: Replace this with your real tests.
+    test('it works', function (assert) {
+        let result = getIssueTypes();
+        assert.ok(result);
+    });
+});
