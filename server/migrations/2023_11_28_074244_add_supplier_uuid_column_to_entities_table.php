3c497e8bbee64c7c572c44ee5deffad87eb8fc45
Doljinsuren Enkhbayar <doljko927@gmail.com>
2023-11-28 15:46:42 +0800

    hotfix patched `DriverObserver` and added the `supplier_uuid` column for entities table

---
+++ server/migrations/2023_11_28_074244_add_supplier_uuid_column_to_entities_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSupplierUuidColumnToEntitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('entities', function (Blueprint $table) {
+            $table->foreignUuid('supplier_uuid')->nullable()->after('driver_assigned_uuid')->references('uuid')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('entities', function (Blueprint $table) {
+            $table->dropForeign(['supplier_uuid']);
+            $table->dropColumn(['supplier_uuid']);
+        });
+    }
+}
