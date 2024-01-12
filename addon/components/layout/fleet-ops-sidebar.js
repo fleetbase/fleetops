188010f225f608319656e2778a1485e0a165424d
Ronald A. Richardson <me@ron.dev>
2024-01-12 17:52:49 +0800

    added new fleet listing sidebar component for next release, bumped version to v0.3.8

--- addon/components/layout/fleet-ops-sidebar.js
+++ addon/components/layout/fleet-ops-sidebar.js
@@ -3,6 +3,7 @@ import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import { isArray } from '@ember/array';
+// import FleetListingComponent from './fleet-ops-sidebar/fleet-listing';
 
 /**
  * LayoutFleetOpsSidebarComponent
