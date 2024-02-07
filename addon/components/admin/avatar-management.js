b591050c5bd50dccd203d06754124ec525f85bfe
TemuulenBM <temuulen.developer@gmail.com>
2024-02-07 18:00:46 +0800

    getting files within type and renders it on the component

--- addon/components/admin/avatar-management.js
+++ addon/components/admin/avatar-management.js
@@ -2,7 +2,7 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
-
+import { task } from 'ember-concurrency';
 export default class AdminAvatarManagementComponent extends Component {
     /**
      * Inject the `fileQueue` service
@@ -13,11 +13,22 @@ export default class AdminAvatarManagementComponent extends Component {
 
     @service fetch;
 
+    @service notifications;
+
     @tracked uploadQueue = [];
+
+    @tracked selectedCategory = null;
+
+    @tracked filteredFiles = [];
 
     constructor() {
         super(...arguments);
-        this.order = { files: [] };
+        this.avatar = { files: [] };
+    }
+
+    @action selectCategory(category) {
+        this.selectedCategory = category;
+        this.loadFilesByType.perform();
     }
 
     @action queueFile(file) {
@@ -25,15 +36,34 @@ export default class AdminAvatarManagementComponent extends Component {
             return;
         }
 
+        let uploadPath;
+        let uploadType;
+        switch (this.selectedCategory) {
+            case 'vehicles':
+                uploadPath = '/custom-avatars/vehicles';
+                uploadType = 'vehicle_avatar';
+                break;
+            case 'places':
+                uploadPath = '/custom-avatars/places';
+                uploadType = 'place_avatar';
+                break;
+            case 'drivers':
+                uploadPath = '/custom-avatars/drivers';
+                uploadType = 'driver_avatar';
+                break;
+            default:
+                throw new Error('Invalid category selected');
+        }
+
         this.uploadQueue.pushObject(file);
         this.fetch.uploadFile.perform(
             file,
             {
-                path: '/custom_avatars',
-                type: 'custom_avatar',
+                path: uploadPath,
+                type: uploadType,
             },
             (uploadedFile) => {
-                this.order.files.pushObject(uploadedFile);
+                this.avatar.files.pushObject(uploadedFile);
                 this.uploadQueue.removeObject(file);
             },
             () => {
@@ -48,5 +78,21 @@ export default class AdminAvatarManagementComponent extends Component {
 
     @action removeFile(file) {
         return file.destroyRecord();
+    }
+
+    @task *loadFilesByType() {
+        let type = {
+            vehicles: 'vehicle_avatar',
+            places: 'place_avatar',
+            drivers: 'driver_avatar',
+        }[this.selectedCategory];
+        const filteredFiles = yield this.fetch.get('files', { type: type }).catch((error) => {
+            this.notifications.serverError(error);
+        });
+        if (filteredFiles) {
+            this.filteredFiles = filteredFiles;
+        }
+
+        return filteredFiles;
     }
 }
