ad107129d929488f22e7943c793cd3b148d400b0
Doljinsuren Enkhbayar <doljko927@gmail.com>
2024-03-04 13:55:23 +0800

    aded api

--- server/src/Http/Controllers/Api/v1/IssueController.php
+++ server/src/Http/Controllers/Api/v1/IssueController.php
@@ -2,10 +2,17 @@
 
 namespace Fleetbase\FleetOps\Http\Controllers\Api\v1;
 
+use Fleetbase\FleetOps\Http\Requests\CreateIssueRequest;
+use Fleetbase\FleetOps\Http\Requests\UpdateIssueRequest;
+use Fleetbase\FleetOps\Http\Resources\v1\Fleet as FleetResource;
 use Fleetbase\FleetOps\Http\Resources\v1\Issue as IssueResource;
+use Fleetbase\FleetOps\Http\Resources\v1\Issue as DeletedIssue;
 use Fleetbase\FleetOps\Models\Driver;
 use Fleetbase\FleetOps\Models\Issue;
+use Fleetbase\FleetOps\Support\Utils;
+use Fleetbase\FleetOps\Models\Fleet;
 use Fleetbase\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class IssueController extends Controller
 {
@@ -16,7 +23,7 @@ class IssueController extends Controller
      *
      * @return \Fleetbase\Http\Resources\Entity
      */
-    public function create(CreateIssueRequest $request)
+    public function createRecord(CreateIssueRequest $request)
     {
         // get request input
         $input = $request->only([
@@ -24,6 +31,7 @@ class IssueController extends Controller
             'location',
             'category',
             'type',
+            'report',
             'priority',
         ]);
 
@@ -49,5 +57,85 @@ class IssueController extends Controller
 
         // response the driver resource
         return new IssueResource($entity);
+    }
+
+       /**
+     * Updates new Fleetbase Issue resource.
+     * 
+     * @param string                                      $id
+     * @param \Fleetbase\Http\Requests\UpdateIssueRequest $request
+     *
+     * @return \Fleetbase\Http\Resources\Issue
+     */
+    public function update($id, UpdateIssueRequest $request)
+    {
+        // find for the fleet
+        try {
+            $fleet = Issue::findRecordOrFail($id);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $exception) {
+            return response()->json(
+                [
+                    'error' => 'Fleet resource not found.',
+                ],
+                404
+            );
+        }
+
+        // get request input
+        $input = $request->only(['name']);
+
+        // service area assignment
+        if ($request->has('service_area')) {
+            $input['service_area_uuid'] = Utils::getUuid('service_areas', [
+                'public_id'    => $request->input('service_area'),
+                'company_uuid' => session('company'),
+            ]);
+        }
+
+        // update the fleet
+        $fleet->update($input);
+
+        // response the fleet resource
+        return new FleetResource($fleet);
+    }
+
+
+    /**
+     * Query for Fleetbase Issue resources.
+     *
+     * @return \Fleetbase\Http\Resources\FleetCollection
+     */
+    public function queryRecord(Request $request)
+    {
+        $results = Issue::queryWithRequest($request);
+
+        return IssueResource::collection($results);
+    }
+
+
+        /**
+     * Deletes a Fleetbase Issue resources.
+     *
+     * @return \Fleetbase\Http\Resources\FleetCollection
+     */
+    public function delete($id)
+    {
+        // find for the driver
+        try {
+            $issue = Issue::findRecordOrFail($id);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $exception) {
+            return response()->json(
+                [
+                    'error' => 'Issue resource not found.',
+                ],
+                404
+            );
+        }
+
+        // delete the contact
+        $issue->delete();
+
+        // response the contact resource
+        return new DeletedIssue($issue);
     }
 }
