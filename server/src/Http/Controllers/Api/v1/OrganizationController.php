80e05a97eea5150fb2fda0dc0a88cbfa50bac26b
Doljinsuren Enkhbayar <doljko927@gmail.com>
2024-03-21 15:36:01 +0800

    response returned array

--- server/src/Http/Controllers/Api/v1/OrganizationController.php
+++ server/src/Http/Controllers/Api/v1/OrganizationController.php
@@ -2,6 +2,7 @@
 
 namespace Fleetbase\FleetOps\Http\Controllers\Api\v1;
 
+use Illuminate\Support\Collection;
 use Fleetbase\Http\Controllers\Controller;
 use Fleetbase\Http\Resources\Organization;
 use Fleetbase\Models\Company;
@@ -23,25 +24,24 @@ class OrganizationController extends Controller
         $companies = Company::whereHas('users')->get()->map(function ($company) {
             return [
                 'name' => $company->name,
-                'uuid' => $company->uuid,
+                'uuid'   => $company->uuid,
             ];
         });
     
         if ($withDriverOnboardEnabled) {
-            $driverOnboardSettings = Setting::where('key', 'fleet-ops.driver-onboard-settings')->value('value');
+            $driverOnboardSettings  = Setting::where('key', 'fleet-ops.driver-onboard-settings')->value('value');
     
             $companies = $companies->filter(function ($company) use ($driverOnboardSettings) {
-                // Check if $company is an array and has 'uuid' key
-                return is_array($company) && array_key_exists('uuid', $company) &&
-                    $driverOnboardSettings && isset($driverOnboardSettings[$company['uuid']]) &&
-                    data_get($driverOnboardSettings[$company['uuid']], 'enableDriverOnboardFromApp') === true;
+                return $driverOnboardSettings && isset($driverOnboardSettings[$company['uuid']]) && data_get($driverOnboardSettings[$company["uuid"]], 'enableDriverOnboardFromApp') === true;
             });
         }
-
-        info("Test", [$companies]);
+    
         // limit
         $companies = $companies->take($limit);
-
-        return response()->json($companies);
+    
+        $companiesArray = $companies->values()->toArray();
+    
+        return response()->json($companiesArray);
     }
+    
 }
