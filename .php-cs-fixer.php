b85cfe5760c1328dd22c474ce1db0a7071b26ea7
Ronald A. Richardson <me@ron.dev>
2024-05-29 12:08:14 +0800

    updated php cs fixer with `phpdoc_to_comment` rule as false

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -10,6 +10,7 @@ $finder = PhpCsFixer\Finder::create()
 $rules = [
     '@Symfony'               => true,
     'phpdoc_no_empty_return' => false,
+    'phpdoc_to_comment'      => false,
     'array_syntax'           => ['syntax' => 'short'],
     'yoda_style'             => false,
     'binary_operator_spaces' => [
