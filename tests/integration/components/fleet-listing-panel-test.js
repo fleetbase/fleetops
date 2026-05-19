4e7544004967b4fa48897bfc2250750cd261adaa
Ronald A. Richardson <me@ron.dev>
2026-05-19 14:38:54 +0800

    Fix subfleet vehicle rendering in sidebar

--- tests/integration/components/fleet-listing-panel-test.js
+++ tests/integration/components/fleet-listing-panel-test.js
@@ -6,21 +6,50 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | fleet-listing-panel', function (hooks) {
     setupRenderingTest(hooks);
 
-    test('it renders', async function (assert) {
-        // Set any properties with this.set('myProperty', 'value');
-        // Handle any actions with this.set('myAction', function(val) { ... });
+    test('it renders subfleet vehicles and direct-only counts', async function (assert) {
+        this.set('fleet', {
+            name: 'EastLink',
+            vehicles_online_count: 1,
+            vehicles_count: 1,
+            vehicles: [
+                {
+                    displayName: '2025 Nissan NV200',
+                    online: true,
+                },
+            ],
+            subfleets: [
+                {
+                    name: 'Changi Cargo',
+                    vehicles_online_count: 3,
+                    vehicles_count: 3,
+                    vehicles: [
+                        {
+                            displayName: 'CGO-05',
+                            online: true,
+                        },
+                        {
+                            displayName: 'CGO-16',
+                            online: true,
+                        },
+                        {
+                            displayName: 'CGO-17',
+                            online: true,
+                        },
+                    ],
+                    subfleets: [],
+                },
+            ],
+        });
 
-        await render(hbs`<FleetListingPanel />`);
+        await render(hbs`<FleetListingPanel @fleet={{this.fleet}} @depth={{1}} />`);
 
-        assert.dom(this.element).hasText('');
-
-        // Template block usage:
-        await render(hbs`
-      <FleetListingPanel>
-        template block text
-      </FleetListingPanel>
-    `);
-
-        assert.dom(this.element).hasText('template block text');
+        assert.dom(this.element).includesText('EastLink');
+        assert.dom(this.element).includesText('1/1');
+        assert.dom(this.element).includesText('2025 Nissan NV200');
+        assert.dom(this.element).includesText('Changi Cargo');
+        assert.dom(this.element).includesText('3/3');
+        assert.dom(this.element).includesText('CGO-05');
+        assert.dom(this.element).includesText('CGO-16');
+        assert.dom(this.element).includesText('CGO-17');
     });
 });
