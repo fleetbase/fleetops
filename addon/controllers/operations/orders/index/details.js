c397ffdc72b5a212db9535ff9a06a74c7690b7bc
Ronald A. Richardson <me@ron.dev>
2026-05-26 19:19:46 +0800

    Fix lint failures

--- addon/controllers/operations/orders/index/details.js
+++ addon/controllers/operations/orders/index/details.js
@@ -106,7 +106,7 @@ export default class OperationsOrdersIndexDetailsController extends Controller {
                         text: this.model.has_driver_assigned ? 'Unassign driver' : 'Assign driver',
                         icon: this.model.has_driver_assigned ? 'user-xmark' : 'edit',
                         disabled: this.model.has_driver_assigned ? !this.model.hasActiveStatus || !this.model.driver_assigned : !this.model.hasActiveStatus,
-                        fn: () => this.model.has_driver_assigned ? this.orderActions.unassignDriver(this.model) : this.orderActions.assignDriver(this.model),
+                        fn: () => (this.model.has_driver_assigned ? this.orderActions.unassignDriver(this.model) : this.orderActions.assignDriver(this.model)),
                     },
                     {
                         text: 'View order label',
