e9660fb67dfbd81dc5705caffe0ecb1dc2e47b72
Ronald A. Richardson <me@ron.dev>
2026-07-17 14:14:03 +0800

    Run backend tests file by file in CI

---
+++ scripts/pest-file-runner.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+$autoloadCandidates = [
+    getcwd() . '/server_vendor/autoload.php',
+    getcwd() . '/vendor/autoload.php',
+];
+
+$autoload = null;
+foreach ($autoloadCandidates as $candidate) {
+    if (is_file($candidate)) {
+        $autoload = $candidate;
+        break;
+    }
+}
+
+if ($autoload === null) {
+    fwrite(STDERR, "Unable to find Composer autoload. Run composer install first.\n");
+    exit(1);
+}
+
+require_once $autoload;
+
+use Symfony\Component\Process\Exception\ProcessTimedOutException;
+use Symfony\Component\Process\Process;
+
+$runner = getcwd() . '/scripts/pest-runner.php';
+if (!is_file($runner)) {
+    fwrite(STDERR, "Unable to find Pest runner at scripts/pest-runner.php.\n");
+    exit(1);
+}
+
+$testsPath = getcwd() . '/server/tests';
+$files     = glob($testsPath . '/*.php') ?: [];
+sort($files);
+
+if ($files === []) {
+    fwrite(STDERR, "Unable to find Pest test files in server/tests.\n");
+    exit(1);
+}
+
+$args    = array_slice($argv, 1);
+$timeout = (float) (getenv('PEST_FILE_TIMEOUT') ?: 120);
+
+foreach ($files as $file) {
+    $relativeFile = str_replace(getcwd() . '/', '', $file);
+
+    fwrite(STDOUT, "::group::{$relativeFile}\n");
+
+    $process = new Process(array_merge([PHP_BINARY, $runner], $args, [$relativeFile]), getcwd());
+    $process->setTimeout($timeout);
+    $process->setIdleTimeout($timeout);
+
+    try {
+        $process->run(static function (string $type, string $buffer): void {
+            fwrite($type === Process::ERR ? STDERR : STDOUT, $buffer);
+        });
+    } catch (ProcessTimedOutException $exception) {
+        fwrite(STDERR, "\nTimed out after {$timeout} seconds while running {$relativeFile}.\n");
+        fwrite(STDOUT, "::endgroup::\n");
+        exit(1);
+    }
+
+    fwrite(STDOUT, "::endgroup::\n");
+
+    if (!$process->isSuccessful()) {
+        exit($process->getExitCode() ?: 1);
+    }
+}
