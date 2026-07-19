51a807ebcc66bdeda596a9c6f63d3be60d472e44
Ronald A. Richardson <me@ron.dev>
2026-07-19 10:40:35 +0800

    Fix event dispatch trait imports

--- server/src/Events/EntityActivityChanged.php
+++ server/src/Events/EntityActivityChanged.php
@@ -8,7 +8,7 @@ use Fleetbase\FleetOps\Models\Order;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Carbon;
 
